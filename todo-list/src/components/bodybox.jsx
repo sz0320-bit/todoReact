ac903afdd35c220618ae27b1dc0b6d0c5a421b7f
sz0320-bit <yknse27@gmail.com>
2022-07-01 16:53:39 -0400

    fixed issues, need to fix search bar reset and add edit functionality

--- todo-list/src/components/bodybox.jsx
+++ todo-list/src/components/bodybox.jsx
@@ -1,23 +1,17 @@
 import '../main.css'
 import {Tasks} from './tasks'
-let tasks = JSON.parse(localStorage.getItem('items')) || [];
 const BodyBox = () => {
 
-    const submitBox = () => {
-        console.log(localStorage.getItem('items'));
-    }
+
+
 
     return (
         <div className="glass">
             <div className="bigcontainer">
-                <div className="textenter">
-                    <h2>Please Enter Task:</h2>
-                    <input type="text" id="mainentry"/>
-                        <input type="button" value="submit" id="mainsubmit" onClick={submitBox}/>
-                </div>
-                <div className="displaybox">
-                    <Tasks tasks={tasks}></Tasks>
-                </div>
+
+
+                    <Tasks></Tasks>
+
             </div>
         </div>
     )
--- todo-list/src/components/tasks.jsx
+++ todo-list/src/components/tasks.jsx
@@ -1,47 +1,50 @@
 import {Task} from "./task";
 import {useEffect, useState} from "react";
+let tasks = JSON.parse(localStorage.getItem('items')) || [];
+let task1 = ['task1', 'task2', 'task3'];
 
 
 
 
-const Tasks = ({tasks}) => {
+const Tasks = () => {
 
     const [task, setTasks] = useState(tasks);
-
-const newTask = () => {
-    setTasks(task.concat('newtask'));
-}
-
-useEffect(() => {
-     {
-        tasks = task;
-        localStorage.setItem("items", JSON.stringify(tasks));
-        console.log(task)
-        console.log(tasks);
+    const [text, setText] = useState('');
+    const newTask = (e) => {
+        e.preventDefault();
+        setTasks(task.concat(text));
     }
-}, [task]);
 
-    let count = 0;
-    const counter = () => {
-        count++;
-        return count;
-    }
+    useEffect(() => {
+        {
+            tasks = task;
+            localStorage.setItem("items", JSON.stringify(task));
+            console.log(task)
+            console.log(tasks);
+        }
+    }, [task]);
 
     const deleteTask = (key) => {
         setTasks(task.filter((task, index) => index !== key));
     }
 
     return (
-        <div id="entrypoint">
-            {
-                task.map((task,index) => (
-                    <Task index={index} key={index} task={task+counter()} onDelete={deleteTask}></Task>
-                ))
-            }
-            <input type={"button"} value={'press'} onClick={newTask} />
+        <>
+            <form onSubmit={newTask} className={'textenter'}>
+                <h2>Please Enter Task:</h2>
+                <input type="text" id="mainentry" onChange={(e) => setText(e.target.value)} />
+                <input type="submit" value="submit" id="mainsubmit" />
+            </form>
+        <div className="displaybox">
+            <div id={'entrypoint'}>
+                {
+                    task.map((task, index) => (
+                    <Task index={index} key={index} task={task} onDelete={deleteTask} />
+                ))}
+            </div>
         </div>
+        </>
     )
-
  }
 
  export {Tasks};
