2c6204f9fb0252da6967cc92e33d5846957cfc4a
sz0320-bit <yknse27@gmail.com>
2022-07-06 19:46:22 -0400

    first draft done

--- todo-list/src/components/task.jsx
+++ todo-list/src/components/task.jsx
@@ -27,13 +27,13 @@ const Task = ({task,onDelete,onEdit}) => {
     }, []);
 
     return (
-        <div className="h-fit p-3 flex gap-3 flex-col border-2 border-blue-800 mt-5 rounded-xl shadow-2xl">
+        <div className="h-fit p-3 flex gap-3 flex-col mt-6 bg-white rounded-xl shadow-xl">
             <div className="unline" id="line" >
                 <input ref={ref} type="text" className=" w-[100%] text-center h-fit shadow-none bg-transparent overflow-y-auto break-words font-mono text-xl" readOnly={edit}  value={text} onChange={(e) => setText(e.target.value)}/>
             </div>
-            <div className="flex justify-evenly" >
-                <input type="button" value="edit" className="border-2 px-7 border-blue-800 h-fit px-2 rounded-xl" onClick={handleClick}/>
-                <input type="button" value="delete" className="border-2 px-7 border-blue-800 h-fit px-2 rounded-xl" onClick={() => onDelete(task.id)}/>
+            <div className="flex justify-center gap-5" >
+                <input type="button" value="edit" className=" px-7 font-mono text-xl bg-blue-600 w-32 text-white border-blue-800 h-fit px-2 rounded-xl shadow-xl" onClick={handleClick}/>
+                <input type="button" value="delete" className=" px-7 font-mono text-xl bg-blue-600 w-32 text-white  h-fit px-2 rounded-xl shadow-xl" onClick={() => onDelete(task.id)}/>
             </div>
         </div>
     )
--- todo-list/src/components/tasks.jsx
+++ todo-list/src/components/tasks.jsx
@@ -54,7 +54,7 @@ try {
 
     return (
         <>
-            <form onSubmit={newTask} className={'flex font-mono shadow-2xl border-2 justify-evenly items-center rounded-xl p-2 border-blue-800'}>
+            <form onSubmit={newTask} className={'flex font-mono sticky shadow-2xl border-2 justify-evenly items-center bg-white rounded-xl p-2 '}>
                 <h2 >Please Enter Task:</h2>
                 <input type="text" id="mainentry" className={'font-mono  mr-[1.5em] text-xl border-2 border-blue-800 rounded-xl text-center w-[100%]'} ref={ref} onChange={(e) => setText(e.target.value)}/>
                 <input type="submit" value="submit" className={'border-2 border-blue-800 h-fit px-2 rounded-xl'} id="mainsubmit"/>
