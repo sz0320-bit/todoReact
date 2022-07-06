a3dc1a9d94cbfaa82abbb63cbe0d36e6968aafe8
sz0320-bit <yknse27@gmail.com>
2022-07-06 18:20:05 -0400

    started set up

--- todo-list/src/components/head.jsx
+++ todo-list/src/components/head.jsx
@@ -2,7 +2,7 @@
 const MainHead = () => {
     return (
 
-        <div className="text-center  border-red-500 bg-white rounded-2xlß  shadow-xl text-5xl font-mono text-blue-600 p-2 px-3 mt-3">
+        <div className="text-center  border-red-500 bg-white rounded-2xl  shadow-xl text-5xl font-mono text-blue-600 p-2 px-3 mt-3">
             <a href="index.html" >
                 <h1>TO<span>DO</span>-LIST</h1>
             </a>
